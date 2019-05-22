920fe6a6468ed8f6bb67a169f32242583f838179
ado <ado@DESKTOP-Q6TOGI1>
2019-05-22 15:09:31 +0200

    adding first class vectors

---
+++ testArray/src/testArray/vectors.java
@@ -0,0 +1,11 @@
+package testArray;
+import java.util.Vector;
+public class vectors {
+	private Vector <Integer> a = new Vector <Integer> ();
+	private int s;
+	public vectors (Vector <Integer> a)
+	{
+	
+	}
+
+}
